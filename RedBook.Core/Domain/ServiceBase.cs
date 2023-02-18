bcab10a76c0f3a88bc2bdd1ea60dda35e50bd601
Md. Nafis Sadik <107624814+nafis-sadik-23@users.noreply.github.com>
2023-02-18 20:09:14 +0600

    BaseService Created

---
+++ RedBook.Core/Domain/ServiceBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using RedBook.Core.AutoMapper;
+using RedBook.Core.UnitOfWork;
+
+namespace RedBook.Core.Domain
+{
+    public class ServiceBase
+    {
+        private IUnitOfWorkManager _unitOfWorkManager;
+        public IUnitOfWorkManager UnitOfWorkManager
+        {
+            get
+            {
+                if (_unitOfWorkManager == null)
+                    throw new ArgumentException("UnitOfWorkManager must be assigned before used.");
+
+                return _unitOfWorkManager;
+            }
+            set { _unitOfWorkManager = value; }
+        }
+        public ILogger Logger { protected get; set; }
+        public IObjectMapper Mapper { get; set; }
+        public ServiceBase(ILogger logger, IObjectMapper mapper/*, IClaimsPrincipalAccessor accessor*/)
+        {
+
+        }
+    }
+}
