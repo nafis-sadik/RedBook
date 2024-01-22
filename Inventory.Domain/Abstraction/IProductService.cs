1cd001ebba7054eedab1efc6e44aafbeeb39f96a
nafis-sadik-sebpo <mdnafis.sadik@sebpo.com>
2024-01-22 21:17:40 +0600

    Working on product module

---
+++ Inventory.Domain/Abstraction/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Inventory.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Domain.Abstraction
+{
+    public interface IProductService
+    {
+        public Task<CategoryModel> AddSubcategoryAsync(ProductModel categoryModel);
+        public Task<IEnumerable<CategoryModel>> GetSubcategoriesUnderCategory(int categoryId);
+        public Task DeleteSubcategoryAsync(int categoryId);
+        public Task<CategoryModel> UpdateSubcategoryAsync(CategoryModel categoryModel);
+    }
+}
---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.WebAPI.Controllers
+{
+    /// <summary>
+    /// Product Module
+    /// </summary>
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
