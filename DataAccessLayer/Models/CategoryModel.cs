aa4a6fe75517987b5952f7e8092dfb20e9864f92
Nafis Sadik <nafis_sadik@outlook.com>
2024-04-25 00:45:29 +0600

    Update Database on Update By data type

--- DataAccessLayer/Models/CategoryModel.cs
+++ DataAccessLayer/Models/CategoryModel.cs
@@ -12,10 +12,10 @@
 
         public DateTime CreateDate { get; set; }
 
-        public string CreatedBy { get; set; }
+        public int CreatedBy { get; set; }
 
         public DateTime? UpdateDate { get; set; }
 
-        public string? UpdatedBy { get; set; }
+        public int? UpdatedBy { get; set; }
     }
 }
--- DataAccessLayer/Models/CommonAttributeModel.cs
+++ DataAccessLayer/Models/CommonAttributeModel.cs
@@ -10,10 +10,10 @@
 
         public DateTime CreateDate { get; set; }
 
-        public string CreatedBy { get; set; }
+        public int CreatedBy { get; set; }
 
         public DateTime? UpdateDate { get; set; }
 
-        public string UpdateBy { get; set; }
+        public int? UpdateBy { get; set; }
     }
 }
